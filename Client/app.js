15afb378585352646a77822b40bc4057b257c421
angajasinghe@outlook.com <angajasinghe@gmail.com>
2020-04-26 17:34:42 +0530

    Update Project according to lab 10 MVC

--- Client/app.js
+++ Client/app.js
@@ -1,103 +1,69 @@
+$(document).ready(function() {
+    Init();
+});
 
-
-var Model = (function name() {
-    var OnGetCompleate = function (response) {
-        var resultSet = response;
-
-        if (resultSet.response_status === 1) {
-            //console.log(response.responseJSON);
-            return true;
-
-        }
-        else if (resultSet.response_status === 0) {
-            console.log("it works with eror");
-        }
+//CLIENT-MODEL===================================================================================
+function  validateInit(response) {
+    var resultSet = response;
+    if (resultSet.response_status === 0) {
+        console.log(resultSet);
+        return "Healthcare Doctor Service is Down at the morment";
     }
+    return true;     
+}
 
-    return{
-        efg : function(response,status){
-            return OnGetCompleate(response,status)
-        }
-    }
+//CONTROLLER=====================================================================================
+function Init() {  
+    $.ajax({
+        type: "GET",
+        url: "http://localhost:8080/doctors/webapi/doc",
+        dataType: "json",
+        success: function(response){
+            //
+            if(validateInit(response)){
+                InitUI(response.doc_list);
+            }
+        },
+        error: function (jqXhr, textStatus, errorMessage) {}
+    });
+}
 
-
-
-})();
-
-
-var UIController = (function () {
-
+//UI CONTROLLER==================================================================================
+function InitUIElement(){
     var DOMobj = {
         grid : $("#doc_grid")
     };
 
-    var setGrid = function (jsonList) {
-        var tr = '';
-        $.each(jsonList,function (key,value) { 
-            tr = $('<tr/>');
-            tr.append('<td>'+value.doc_id+'</td>');
-            tr.append('<td>'+value.doc_reg_no+'</td>');
-            tr.append('<td>'+value.specification_id+'</td>');
-            tr.append('<td>'+value.doc_first_name+'</td>');
-            tr.append('<td>'+value.doc_last_name+'</td>');
-            tr.append('<td>'+value.doc_email+'</td>');
-            tr.append('<td>'+value.doc_tp1+'</td>');
-            tr.append('<td>'+value.doc_tp2+'</td>');
-            value.doc_tp3 = value.doc_tp3 == null ? '<small><i>not available</i></small>' : value.doc_tp3;
-            tr.append('<td>'+value.doc_tp3 +'</td>');
-            tr.append('<td>'+value.doc_address+'</td>');
-            tr.append('<td>'+value.doc_city+'</td>');
-            tr.append('<td>'+value.doc_status_id+'</td>');
-            DOMobj.grid.append(tr);
-        });
-    }
+    return DOMobj;
+}
 
-    return{
-        Grid : function(jsonList) {
-            return setGrid(jsonList);
-        }
-    }
+ function InitUI(jsonList) {
+    var tr = '';
+    $.each(jsonList,function (key,value) { 
+        tr = $('<tr/>');
+        tr.append('<td>'+value.doc_id+'</td>');
+        tr.append('<td>'+value.doc_reg_no+'</td>');
+        tr.append('<td>'+value.specification_id+'</td>');
+        tr.append('<td>'+value.doc_first_name+'</td>');
+        tr.append('<td>'+value.doc_last_name+'</td>');
+        tr.append('<td>'+value.doc_email+'</td>');
+        tr.append('<td>'+value.doc_tp1+'</td>');
+        tr.append('<td>'+value.doc_tp2+'</td>');
+        value.doc_tp3 = value.doc_tp3 == null ? '<small><i>not available</i></small>' : value.doc_tp3;
+        tr.append('<td>'+value.doc_tp3 +'</td>');
+        tr.append('<td>'+value.doc_address+'</td>');
+        tr.append('<td>'+value.doc_city+'</td>');
+        tr.append('<td>'+value.doc_status_id+'</td>');
+        InitUIElement().grid.append(tr);
+    });
+    return;
 
-
-})();
-
-
-var Controller = (function (Mod,UICrtl) {
-    var url = {
-        "getAllDocters": "http://localhost:8080/doctors/webapi/doc"
-    };
-    var getAllDoc = function () {
-        $.ajax({
-            type: "GET",
-            url: url.getAllDocters,
-            dataType: "json",
-            success: function(response){
-               if(Mod.efg(response)){
-                   UICrtl.Grid(response.doc_list);
-               }
-            },
-            error: function (jqXhr, textStatus, errorMessage) {}
-        });
-    }
+};
 
 
 
-    
 
 
-    return {
-        init: function () {
-            console.log('Application is started');
-            getAllDoc();
-        }
-    }
-
-})(Model,UIController);
-
-
-$(document).ready(function(CM) {
-    Controller.init();
-});
 
 
 
